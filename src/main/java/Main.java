78d4e88faeb39fc94300549f1cb3f49677626ebe
Neida Veizaga Ochoa <Neida Veizaga@jalasoft.local>
2020-03-27 14:58:44 -0400

    commit 01

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,40 +7,18 @@ public class Main {
 
         // User details
         User userOne = new User("Jerry","BO10006789");
+        userOne.setInitialBalance(5000);
 
         // Welcome to the BankApp
         Bank bankApp = new Bank(userOne);
-        bankApp.welcome();
-        bankApp.currentAccount();
+        bankApp.runBankApp();
 
         // Main Menu
-        //System.out.println(lineSeparator());
-        System.out.println("********************");
-        System.out.println("(W) Withdraw");
-        System.out.println("(D) Deposit");
-        System.out.println("(B) My Balance");
-        System.out.println("(T) My Transactions");
-        System.out.println("(E) Exit");
-        System.out.println("********************");
+        bankApp.menu();
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            char option;
-            do {
-                System.out.print("Select an option: ");
-                option = (char) scanner.nextLine().trim().toUpperCase().charAt(0);
-                switch (option) {
-                    case 'E':
-                        System.out.println("Exit...");
-                        break;
-                    default:
-                        System.out.println(option + " is an invalid option");
-                        break;
-                }
-                System.out.println(System.lineSeparator());
-                System.out.println("(A) Show All options");
-            } while (option != 'E');
+        // Press a key to select an option
+        bankApp.pressKey();
 
-        }
     }
 
 }
